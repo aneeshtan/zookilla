ec681b2e4531497536017b5761411b7948a0302a
Farshad Ghazanfari <aneeshtan@gmail.com>
2023-11-25 12:52:43 +0400

    back

--- constants/websocket.js
+++ constants/websocket.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 
 // const ENDPOINT = 'http://localhost:5000/'
 const ENDPOINT =
-  "http://localhost:5000/";
+  "https://zookilla-npat-dfa754f96096.herokuapp.com/";
 const socket = io(ENDPOINT);
 
 export { socket };
